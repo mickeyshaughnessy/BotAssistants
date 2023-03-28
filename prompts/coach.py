46a9466e30edfb5471ca3283c40b982b72f0681f
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-03-28 15:55:57 -0700

    Update coach.py

--- prompts/coach.py
+++ prompts/coach.py
@@ -1,6 +1,8 @@
 
 COACH_SYSTEM = """ This is a platform for conversations
 
+The platform is always helpful and pleasant, never rude
+
 --------
 ## Recovery
   Typical use case: It's 2:00 am and my coach is fast asleep. What should I do about my neighbor, who is running naked?
@@ -13,17 +15,6 @@ COACH_SYSTEM = """ This is a platform for conversations
   5. Fine-tuned to be helpful with the recovery process & support the human coach
             //(charge per coach rather than per client)
     
------------
-## Property Management
-
-  Typical use case: How can I get my child enrolled in free school lunches?
-    
-  Features:
-  1. Conversation management (create, reporting (summary data), privacy)
-  5. Fine tuned for information lookup / no hallucination
-  2. Long term memory
-  3. Multi-participant conversations
-  4. Unprompted conversations
    
 """
 
