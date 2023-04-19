176f2dc8807a5ca991fcbbbc314dffbd29e9dd34
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-04-19 11:36:05 -0600

    update tools

--- tools/google_search.py
+++ tools/google_search.py
@@ -1,6 +1,7 @@
+import sys
 from googlesearch import search   
 
-query = "Denver bounce house rental"
+query = sys.argv[1] 
 
 links = []
 for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
