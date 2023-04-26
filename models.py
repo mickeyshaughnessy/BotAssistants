6282a89d242adbe521f6af1133a8c3edda55519b
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-04-26 10:38:54 -0600

    update models

--- models.py
+++ models.py
@@ -5,7 +5,15 @@ redis = redis.StrictRedis()
 
 class User():
     def __init__(self, user_id=None):
-        pass
+        self.user_id = user_id
+        self.data = {}
+
+    def post(self, conversation_id, message):
+        requests.post("localhost:%s/chat/", json.dumps({
+            "message" : message,
+            "conversation_id" : conversation_id
+        })) 
+    
 
 class Conversation():
     def __init__(self, conversation_id=None):
@@ -20,26 +28,33 @@ class Conversation():
         res = redis.hget(config.REDHASH_CONVERSATION, convo_id)
         if res:
             cur = json.loads(res)
+        else:
+            cur = {}
+        return cur 
 
-    def save(self, convo_id):
+    def update(self, update={"text":None}):
         current = redis.hget(config.REDHASH_CONVERSATION, self.convo_id)
+        if current:
+            transcript = json.loads(current).get('transcript')
+            if update.get('text'):
+                transcript.append(update['text'])
+                current['transcript'] = transcript 
+        else:
+            current = {"transcript" : []}
+       
+        redis.hset(config.REDHASH_CONVERSATION, self.convo_id, json.dumps(current))
         
-    ## create, update, or fetch a conversation ##
-    ## the idea is users have privileges to do operations on conversations
-
-    ## create ##
-    redis.hset(config.REDHASH_CONVERSATIONS, conversation_id, "{}")
-    return conversation_id
 
 if __name__ == "__main__":
     # Put tests here, __main__ only runs when models.py is run from command line
     req = {
         "user_id" : "testuser1",
         "conversation_id" : "testconvo1",
-        "action" : "POST testconvo1",
+        "action" : "POST testconvo1 {'text' : 'new message!'}",
     }
 
     user = User(req.get('user_id'))
     convo = Conversation(req.get('conversation_id'))
+    
     
 
