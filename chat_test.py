cc8cbcf82e19cb53210b0af0eac3d2c39c650bf9
Ubuntu <ubuntu@ip-172-31-6-28.ec2.internal>
2023-05-23 19:53:13 +0000

    add coach

--- chat_test.py
+++ chat_test.py
@@ -1,11 +1,12 @@
-import requests, time
+import requests, time, config
 
 user_id = '123'
+conversation_id = 9
 
 while True:
     chat = input("User_%s: " % user_id)
     #resp = requests.post("http://127.0.0.1:8010/reporting", json={"input" : chat, "user_id":user_id})
     #resp = requests.post("http://3.89.10.143:8010/chat", json={"text" : chat, "user_id":user_id})
-    resp = requests.post("http://0.0.0.0:8010/chat", json={"text" : chat, "user_id":user_id, "conversation_id":69})
+    resp = requests.post("http://0.0.0.0:%s/chat" % config.api_port, json={"text" : chat, "user_id":user_id, "conversation_id":conversation_id})
     print(resp.text)
     time.sleep(1)
